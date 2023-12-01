aa23f794026d16a66fb00404cf440ba1ab071cdf
Amna Sadia Korai <murtazaamna117@gmail.com>
2023-12-01 22:29:04 +0500

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,19 @@
+
+function openGmailInbox() {
+    var emailAddress = 'murtazaamna117@gmail.com';
+    
+    var gmailInboxURL = 'https://mail.google.com/mail/u/0/#inbox?compose=new&to=' + emailAddress;
+
+    window.open(gmailInboxURL, '_blank');
+}
+
+//function submitForm() {
+ //   var name = document.getElementById('name').value;
+   // var email = document.getElementById('email').value;
+    //var message = document.getElementById('message').value;
+
+   // var responseElement = document.getElementById('response');
+
+
+    //responseElement.innerHTML = `Thank you, ${name}! Your message has been submitted.`;
+//}
